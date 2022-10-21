a941844e48bf6a6ed35e4d5e836f233f5a45c344
CalvinYAOYAO <13168495217@163.com>
2022-10-21 07:30:53 -0400

    add LoginActivity and SignUpActivity

--- app/src/main/java/com/xiang/otcmedicinedelivery/SignUpActivity.java
+++ app/src/main/java/com/xiang/otcmedicinedelivery/SignUpActivity.java
@@ -4,19 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.*;
+import android.view.View;
 
-public class LoginActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_sign_up);
     }
 
     public void onClickSignUp(View view) {
         //https://stackoverflow.com/questions/13194081/how-to-open-a-second-activity-on-click-of-button-in-android-app
-        Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+        Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
         startActivity(intent);
     }
 }
