d6a50c0190cd6dc9e25d4221242834bec5bb9fec
CalvinYAOYAO <13168495217@163.com>
2022-10-21 20:48:26 -0400

    persist sign up data with firebase

--- app/src/main/java/com/xiang/otcmedicinedelivery/SignUpActivity.java
+++ app/src/main/java/com/xiang/otcmedicinedelivery/SignUpActivity.java
@@ -5,18 +5,65 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.*;
+import android.view.*;
+
+import androidx.annotation.NonNull;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class SignUpActivity extends AppCompatActivity {
+    Button btnSignUp;
+    EditText edtEmail, edtPassword;
+    String txtEmail, txtPassword;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        edtEmail = findViewById(R.id.edtSignUpEmail);
+        edtPassword = findViewById(R.id.edtSignUpPassword);
+        btnSignUp = findViewById(R.id.btnSignUp);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtEmail = edtEmail.getText().toString().trim();
+                txtPassword = edtPassword.getText().toString().trim();
+
+                SignUpUser();
+//                Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
+//                startActivity(intent);
+            }
+        });
     }
 
-    public void onClickSignUp(View view) {
-        //https://stackoverflow.com/questions/13194081/how-to-open-a-second-activity-on-click-of-button-in-android-app
-        Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
-        startActivity(intent);
+    private void SignUpUser() {
+        btnSignUp.setVisibility(View.INVISIBLE);
+
+        mAuth.createUserWithEmailAndPassword(txtEmail, txtPassword).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                Toast.makeText(SignUpActivity.this, "Sign Up Successful !", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(SignUpActivity.this, "Error " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                btnSignUp.setVisibility(View.VISIBLE);
+            }
+        });
+
     }
 }
